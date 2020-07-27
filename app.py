88b2c0f9f4b7e5db5202205eb3f1f7e22fbf264c
daikiakiyoshi <daiki.akiyoshi@outlook.com>
2020-07-27 22:19:48 +0900

    add CORS

--- app.py
+++ app.py
@@ -12,7 +12,16 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
     setup_db(app)
-    CORS(app)
+
+    cors = CORS(app, resources={"*": {"origins": "*"}})
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers',
+                             'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods',
+                             'GET,PATCH,POST,DELETE,OPTIONS')
+        return response
 
     @app.route('/portfolios', methods=['GET'])
     @requires_auth('get:portfolios')
